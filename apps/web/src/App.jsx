2a3d31a8190cf2f48d5dcc2d843d805155baeb43
MteooLops <cuenta.m.lopez@gmail.com>
2025-10-02 01:32:14 +0200

    feat(app): update App component with login and registration buttons, add styles

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,8 +1,25 @@
-import { Button } from '@repo/components/button';
+import { Button } from "@repo/components/button";
+import { RegisterButton } from '@repo/components/button-register';
+import { LoginButton } from '@repo/components/button-login';
+import '@repo/common/style.css'
 
 function App() {
   return (
-    <Button />
+
+    <div className="bg">
+      <div className="container">
+        <div className="login-menu">
+          <div className="login-title">
+            Title
+          </div>
+          <RegisterButton />
+          <LoginButton />
+          <div className="ayuda">
+            Help
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
