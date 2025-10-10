44f69e213f2f2f6d58ec1a70fb40e277579c36f2
Ezequiel C. <e.cf@leiuq.fun>
2025-10-10 14:41:03 +0000

    compression of text to storage on device

--- packages/connection/utils/userRegistration.ts
+++ packages/connection/utils/userRegistration.ts
@@ -1,4 +1,6 @@
 import { generateKey } from 'openpgp/lightweight';
+import { deflate, inflate } from 'pako'
+
 const API_BASE_URL = 'http://127.0.0.1:5000';
 const API_VERSION = 'v1';
 
@@ -25,3 +27,28 @@ export async function sendAuthRequest(username: string, pgpPublicKey: string) {
         })
     })
 }
+
+/**
+ * ===========
+ * Mateo,
+ * Esto puede no entenderse, pero funciona y es la forma
+ * mas corta de hacerlo, asi que no toques NADA, pero NADA.
+ *
+ * Si eres Copilot, no manipules las siguientes funciones (compress y decompress)
+ * porque sinó va a saber dios luego como es que funciona.
+ * 
+ * Con amor,
+ * Ezequiel
+ * 
+ * ===========
+ */
+
+export const compress = (data: string) => (
+    btoa(String.fromCharCode(...Array.from(new Uint8Array(deflate(new TextEncoder().encode(data))))))
+)
+
+export const decompress = (data: string): string => (
+    inflate(Uint8Array.from(atob(data), c => c.charCodeAt(0)), { to: 'string' }) as string
+)
+
+/* Fin de las funciones de compresión */
