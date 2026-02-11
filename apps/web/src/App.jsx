e1a3e38f1141ba4a1471e2c345a14fcae77cb1bd
Ezequiel C. <e.cf@leiuq.fun>
2026-02-11 01:18:02 +0100

    auth condition routes

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,5 +1,5 @@
 import { Toaster } from 'react-hot-toast'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { BrowserRouter, Routes, Route, Link, Navigate, Outlet } from 'react-router'
 
 import '@repo/common/style.css'
 
@@ -9,39 +9,63 @@ import Login from './pages/auth/login'
 import { ChatOverlay } from './pages/chat/index'
 import { ChatRoom } from './pages/chat/[chatId]'
 import { NewChatRoom } from './pages/chat/new'
+import { useAuth } from './hooks/useAuth'
+import { Fragment } from 'react'
 import { AuthProvider } from './providers/AuthProvider'
 
-const General404 = () => (
-  <h1>Page Not Found</h1>
+const General404 = ({ toChats } = { toChats: false }) => (
+  <Fragment>
+    <h1>Page Not Found</h1>
+    <Link to={toChats ? "/chat/global-chat" : "/"} className="text-blue-500 hover:underline">
+      Go back to {toChats ? "Global Chat" : "Home"}
+    </Link>
+  </Fragment>
 )
 
+const ConditionalRoute = ({ condition, expectedValue, redirectTo, element }) => {
+  if(condition === expectedValue) {
+    return element ? element : <Outlet />;
+  }
+
+  return <Navigate to={redirectTo} replace />;
+}
+
 function App() {
+  const { loading, auth } = useAuth();
+  const isAuthenticated = auth.isAuthenticated;
+
+  if(loading) {
+    return (
+      <div className="flex items-center justify-center h-screen w-screen">
+        <h1 className="text-2xl font-bold">Loading...</h1>
+      </div>
+    )
+  }
+
   return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<ConditionalRoute condition={isAuthenticated} expectedValue={false} redirectTo="/chat/global-chat" element={<Home />} />} />
 
-          {/* Auth Routes */}
-          <Route path="/auth">
-            <Route path="register" element={<Register />} />
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<General404 />} />
-          </Route>
-
-          {/* Chat Routes */}
-          <Route path="/chat" element={<ChatOverlay />}>
-            <Route index element={<General404 />} />
-            <Route path="new" element={<NewChatRoom />} />
-            <Route path=":chatId" element={<ChatRoom />} />
-            <Route path="*" element={<General404 />} />
-          </Route>
-
+        {/* Auth Routes */}
+        <Route path="/auth" element={<ConditionalRoute condition={isAuthenticated} expectedValue={false} redirectTo="/chat/global-chat" />}>
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
           <Route path="*" element={<General404 />} />
-        </Routes>
-        <Toaster position='bottom-right' />
-      </BrowserRouter>
-    </AuthProvider>
+        </Route>
+
+        {/* Chat Routes */}
+        <Route path="/chat" element={<ConditionalRoute condition={isAuthenticated} expectedValue={true} redirectTo="/auth/login" element={<ChatOverlay />} />} >
+          <Route index element={<General404 toChats={true} />} />
+          <Route path="new" element={<NewChatRoom />} />
+          <Route path=":chatId" element={<ChatRoom />} />
+          <Route path="*" element={<General404 toChats={true} />} />
+        </Route>
+
+        <Route path="*" element={<General404 toChats={isAuthenticated} />} />
+      </Routes>
+      <Toaster position='bottom-right' />
+    </BrowserRouter>
   )
 }
 
